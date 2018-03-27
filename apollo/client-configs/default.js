45620b955eff3f58160863372a2152b9888813f0
sakilove <tyrantax43@gmail.com>
2018-03-27 23:06:16 +0800

    setup

--- apollo/client-configs/default.js
+++ apollo/client-configs/default.js
@@ -4,7 +4,8 @@ import { InMemoryCache } from 'apollo-cache-inmemory'
 
 
 const API = 'https://api.github.com/graphql'
-const token = 'a6daf3c45d51ea71468' + 'baaa8b16a4dfabe8c54ff' //prevent Github auto detection
+const token = '7143cc00ee41de24eaef' + 'a162d1ee77dbe3a39f2f' //prevent Github auto detection
+
 
 export default () => {
   const httpLink = new HttpLink({ uri: API })
